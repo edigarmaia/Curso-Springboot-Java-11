2ae57d71918ec885848bed1b6941c3f106686f45
Edigar Maia <edigarmaia@gmail.com>
2021-04-17 22:27:56 -0300

    Recursos e entidades do usuario

---
+++ src/main/java/com/webprogram/apiCurso/resources/UsuarioResource.java
@@ -0,0 +1,20 @@
+package com.webprogram.apiCurso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webprogram.apiCurso.entities.Usuario;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class UsuarioResource {
+	//Recursos do Usuario
+	
+	@GetMapping
+	public ResponseEntity<Usuario> findAll(){
+		Usuario u = new Usuario(1L, "Maria", "maria@gmail.com","9999-9999" , "123");
+		return ResponseEntity.ok().body(u);
+	}
+}
