b3f9ec59671697cd873d45a865d0d68e711ba850
Edigar Maia <edigarmaia@gmail.com>
2021-04-17 22:04:46 -0300

    Projeto Criado

---
+++ src/test/java/com/webprogram/apiCurso/ApiCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webprogram.apiCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
